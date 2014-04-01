ed761e8e0f20b5cfd1ec267396c9ef1d5bf97467
mvr05083 <mkdeveloper56@gmail.com>
2014-04-01 17:02:15 -0400

    Update Changes

--- picoinvoices/src/com/pico/picoinvoices/SpinnerAdapter.java
+++ picoinvoices/src/com/pico/picoinvoices/SpinnerAdapter.java
@@ -1,73 +1,60 @@
-package com.pico.picoinvoices;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import android.R.string;
-import android.content.Context;
-import android.content.res.Resources;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class SpinnerAdapter extends ArrayAdapter<String[]>
+import java.util.ArrayList;
+
+public class SpinnerAdapter extends Activity
 {
-    
-   private Context activity;
-   private List<String[]> data;
-   public Resources res;
-   LayoutInflater inflater;
-    
-   /*************  CustomAdapter Constructor *****************/
-   public SpinnerAdapter(Context cxt, List<String[]> list) 
-    {
-       super(cxt, 0);
-        
-       /********** Take passed values **********/
-       activity = cxt;
-       data     = list;
-   
-       /***********  Layout inflator to call external xml layout () **********************/
-       inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        
-     }
+ArrayList<String> name = new ArrayList<String>();
+ArrayList<String> age = new ArrayList<String>();
 
-   @Override
-   public View getDropDownView(int position, View convertView,ViewGroup parent) {
-       return getCustomView(position, convertView, parent);
-   }
+@Override
+public void onCreate(Bundle savedInstanceState)
+{
+super.onCreate(savedInstanceState);
+setContentView(R.layout.customspinner);
+for (int i =0 ;i<=2;i++){
+name.add(""+i);
+age.add(""+i);
+}
+Spinner mySpinner = (Spinner)findViewById(R.id.spinner);
+mySpinner.setAdapter(new MyAdapter(this, R.layout.spinnerrow, name));
+}
 
-   @Override
-   public View getView(int position, View convertView, ViewGroup parent) {
-       return getCustomView(position, convertView, parent);
-   }
+public class MyAdapter extends ArrayAdapter<String>
+{
 
-   // This funtion called for each row ( Called data.size() times )
-   public View getCustomView(int position, View convertView, ViewGroup parent) {
+public MyAdapter(Context context, int textViewResourceId, ArrayList<String> objects)
+{
+super(context, textViewResourceId, objects);
+}
 
-       /********** Inflate spinner_rows.xml file for each row ( Defined below ) ************/
-       View row = inflater.inflate(R.layout.spinner_text_layout, parent, false);
-        
-        
-       TextView label        = (TextView)row.findViewById(R.id.spinnerText1);
-       TextView sub          = (TextView)row.findViewById(R.id.spinnerText2);
-      
-        
-       if(position==0){
-            
-           // Default selected Spinner item 
-           label.setText("Please select company");
-           sub.setText("");
-       }
-       else
-       {
-           // Set values for spinner each row 
-           label.setText(data.get(position)[0].toString());
-           sub.setText(data.get(position)[1].toString());
-       }   
+@Override
+public View getDropDownView(int position, View convertView,ViewGroup parent)
+{
+return getCustomView(position, convertView, parent);
+}
 
-       return row;
-     }
+@Override
+public View getView(int position, View convertView, ViewGroup parent)
+{
+return getCustomView(position, convertView, parent);
+}
+
+public View getCustomView(int position, View convertView, ViewGroup parent)
+{
+
+LayoutInflater inflater=getLayoutInflater();
+View row=inflater.inflate(R.layout.spinnerrow, parent, false);
+TextView label=(TextView)row.findViewById(R.id.company);
+label.setText(name.get(position));
+
+TextView sub=(TextView)row.findViewById(R.id.sub);
+sub.setText(age.get(position));
+
+
+return row;
+}
+}
 }
