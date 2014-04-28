21933b293ac87d2f0c061377c858dd8ba03c4709
kadensotek <bobby23jones@gmail.com>
2014-04-28 18:20:44 -0400

    Implemented CSV parser.  Functioning correctly.  Needs to update database now.

--- picoinvoices/src/com/pico/picoinvoices/ImportDB.java
+++ picoinvoices/src/com/pico/picoinvoices/ImportDB.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 import org.xmlpull.v1.XmlPullParserException;
@@ -13,6 +14,9 @@ import org.xmlpull.v1.XmlPullParserException;
 import com.pico.picoinvoices.XmlImporter.Client;
 import com.pico.picoinvoices.XmlImporter.Invoice;
 import com.pico.picoinvoices.XmlImporter.Service;
+import com.pico.picoinvoices.CsvImporter.ClientCSV;
+import com.pico.picoinvoices.CsvImporter.InvoiceCSV;
+import com.pico.picoinvoices.CsvImporter.ServiceCSV;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -63,7 +67,7 @@ public class ImportDB extends Activity
         _sp.saveClientID("0");
         _sp.saveInvioceID("0");
         openDB();
-        //closeDB();
+        // closeDB();
     }
 
     // @Override
@@ -103,8 +107,7 @@ public class ImportDB extends Activity
 
     public void onClick_ToImportCSV(View v)
     {
-        // Intent intent = new Intent(this, ImportCSV.class);
-        // startActivity(intent);
+        csvConfirmation(v);
     }
 
     // //////////////////////////////////////////////////////
@@ -115,8 +118,8 @@ public class ImportDB extends Activity
 
     public void importFromXML(View v)
     {
-        System.out.println("Importing selected.");
-        
+        System.out.println("Importing from XML selected.");
+
         XmlImporter xmlImporter = null;
         List<Invoice> invoices = null;
         List<Client> clients = null;
@@ -124,20 +127,61 @@ public class ImportDB extends Activity
         InputStream stream = null;
         String path = null;
         this.db = _myDb.getDB();
-        
-        path = Environment.getExternalStorageDirectory() + "/Android/data/com.pico.picoinvoices/";
+
+        path = Environment.getExternalStorageDirectory()
+                + "/Android/data/com.pico.picoinvoices/";
         xmlImporter = new XmlImporter(this.db);
-        stream = getInputStream(stream, path);
-        
-                
+        stream = getInputStream(stream, path, "picodatabase.xml");
+
         try
         {
             invoices = xmlImporter.parseInvoices(stream);
-        }
-        catch (XmlPullParserException e)
+        } catch (XmlPullParserException e)
         {
             // TODO Auto-generated catch block
             e.printStackTrace();
+        } catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        // for (Invoice invoice : invoices)
+        // {
+        // System.out.println("For loop");
+        // invoice.toString();
+        // }
+
+        // File xmlFile = null;
+        // String xmlFilepath = null;
+        // xmlFilepath = getXML();
+        // System.out.println("Filepath1 is " + xmlFilepath);
+
+        // xmlFile = new File(xmlFilepath);
+
+        // System.out.println("Final selected file is " + xmlFile.toString());
+    }
+
+    public void importFromCSV(View v)
+    {
+        System.out.println("Importing from CSV selected.");
+
+        CsvImporter csvImporter = null;
+        List<InvoiceCSV> invoices = null;
+        List<ClientCSV> clients = null;
+        List<ServiceCSV> services = null;
+        String path = Environment.getExternalStorageDirectory() + "/Android/data/com.pico.picoinvoices/";
+        String fileStr = null;
+        this.db = _myDb.getDB();
+
+        csvImporter = new CsvImporter(this.db);
+
+        try
+        {
+            fileStr = readFile(path + "picodatabase.csv");
+            invoices = csvImporter.parseInvoices(new String(fileStr));
+            clients = csvImporter.parseClients(new String(fileStr));
+            services = csvImporter.parseServices(new String(fileStr));
         }
         catch (IOException e)
         {
@@ -145,31 +189,53 @@ public class ImportDB extends Activity
             e.printStackTrace();
         }
         
-//        for (Invoice invoice : invoices)
-//        {
-//            System.out.println("For loop");
-//            invoice.toString();
-//        }
+        for (InvoiceCSV invoice : invoices)
+        {
+            System.out.println(invoice.toString());
+        }
         
+        for(ClientCSV client : clients)
+        {
+            System.out.println(client.toString());
+        }
         
-//        File xmlFile = null;
-//        String xmlFilepath = null;
-//        xmlFilepath = getXML();
-//        System.out.println("Filepath1 is " + xmlFilepath);
-        
-//        xmlFile = new File(xmlFilepath);
-        
-//        System.out.println("Final selected file is " + xmlFile.toString());
+        for(ServiceCSV service : services)
+        {
+            System.out.println(service.toString());
+        }
     }
 
-    private InputStream getInputStream(InputStream stream, String path)
+    private String readFile(String pathname) throws IOException
+    {
+
+        File file = new File(pathname);
+        StringBuilder fileContents = new StringBuilder((int) file.length());
+        Scanner scanner = new Scanner(file);
+        String lineSeparator = System.getProperty("line.separator");
+
+        try
+        {
+            while (scanner.hasNextLine())
+            {
+                fileContents.append(scanner.nextLine() + lineSeparator);
+            }
+            
+            return fileContents.toString();
+        }
+        finally
+        {
+            scanner.close();
+        }
+    }
+
+    private InputStream getInputStream(InputStream stream, String path,
+            String filename)
     {
         /* Opens the xml file as an input stream to be used with xmlImporter */
         try
         {
-            stream = new FileInputStream(new File(path + "picodatabase.xml"));
-        }
-        catch (FileNotFoundException e)
+            stream = new FileInputStream(new File(path + filename));
+        } catch (FileNotFoundException e)
         {
             // TODO Auto-generated catch block; needs to be handled
             System.out.println("File not found");
@@ -177,13 +243,12 @@ public class ImportDB extends Activity
         }
         return stream;
     }
-    
 
     public void xmlConfirmation(final View v)
     {
         new AlertDialog.Builder(this)
                 .setIcon(android.R.drawable.ic_dialog_alert)
-                .setTitle("Confirm export")
+                .setTitle("Confirm import")
                 .setMessage(
                         "Are you sure you want to import the database from XML?")
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener()
@@ -201,7 +266,7 @@ public class ImportDB extends Activity
     {
         new AlertDialog.Builder(this)
                 .setIcon(android.R.drawable.ic_dialog_alert)
-                .setTitle("Confirm export")
+                .setTitle("Confirm import")
                 .setMessage(
                         "Are you sure you want to import the database from CSV?")
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener()
@@ -209,33 +274,33 @@ public class ImportDB extends Activity
                     @Override
                     public void onClick(DialogInterface dialog, int which)
                     {
-                        // importFromCSV(v);
+                        importFromCSV(v);
                     }
 
                 }).setNegativeButton("No", null).show();
     }
 
-//    public String getXML()
-//    {
-//        File dir = new File(Environment.getExternalStorageDirectory(), "");
-//        String xmlFilepath = null;
-//
-//        FileDialog dialog = new FileDialog(this, dir);
-//        dialog.setFileEndsWith(".xml");
-//        dialog.addFileListener(new FileDialog.FileSelectedListener()
-//        {
-//            public void fileSelected(File file)
-//            {
-//                System.out.println("selected file is " + file.toString());
-//            }
-//        });
-//
-//        dialog.showDialog();
-//        xmlFilepath = dialog.getSelectedFilepath();
-//        
-//        System.out.println("Filepath2 is " + xmlFilepath);
-//
-//        return xmlFilepath;
-//    }
+    // public String getXML()
+    // {
+    // File dir = new File(Environment.getExternalStorageDirectory(), "");
+    // String xmlFilepath = null;
+    //
+    // FileDialog dialog = new FileDialog(this, dir);
+    // dialog.setFileEndsWith(".xml");
+    // dialog.addFileListener(new FileDialog.FileSelectedListener()
+    // {
+    // public void fileSelected(File file)
+    // {
+    // System.out.println("selected file is " + file.toString());
+    // }
+    // });
+    //
+    // dialog.showDialog();
+    // xmlFilepath = dialog.getSelectedFilepath();
+    //
+    // System.out.println("Filepath2 is " + xmlFilepath);
+    //
+    // return xmlFilepath;
+    // }
 
 }
